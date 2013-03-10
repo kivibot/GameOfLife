a9170abdb4256516bad47d0e3d9fb6feaa70abdb
kivibot <kivi@kivibot.fi>
2013-03-10 17:46:46 +0000

    added nbproject

--- nbproject/Makefile-Release.mk
+++ nbproject/Makefile-Release.mk
@@ -53,7 +53,7 @@ FFLAGS=
 ASFLAGS=
 
 # Link Libraries and Options
-LDLIBSOPTIONS=
+LDLIBSOPTIONS=`pkg-config --libs sdl`  
 
 # Build Targets
 .build-conf: ${BUILD_SUBPROJECTS}
@@ -66,12 +66,12 @@ ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/gameoflife: ${OBJECTFILES}
 ${OBJECTDIR}/GameOfLife.o: GameOfLife.cpp 
 	${MKDIR} -p ${OBJECTDIR}
 	${RM} $@.d
-	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/GameOfLife.o GameOfLife.cpp
+	$(COMPILE.cc) -O2 `pkg-config --cflags sdl`   -MMD -MP -MF $@.d -o ${OBJECTDIR}/GameOfLife.o GameOfLife.cpp
 
 ${OBJECTDIR}/main.o: main.cpp 
 	${MKDIR} -p ${OBJECTDIR}
 	${RM} $@.d
-	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/main.o main.cpp
+	$(COMPILE.cc) -O2 `pkg-config --cflags sdl`   -MMD -MP -MF $@.d -o ${OBJECTDIR}/main.o main.cpp
 
 # Subprojects
 .build-subprojects:
